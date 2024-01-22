c4186c51d64175aa1c3749eaf4391f1eba056dd2
NikitaGamer64 <nikitakordonov@mail.ru>
2024-01-22 12:37:58 +0300

    Making more corrections

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -47,19 +47,84 @@ namespace TicTacToe
                 }
             }
         }
+        private void TransitionToEndScreen(string text, ImageSource winnerImage)
+        {
+            TurnPanel.Visibility = Visibility.Hidden;
+            GameCanvas.Visibility = Visibility.Hidden;
+            ResultText.Text = text;
+            WinnerImage.Source = winnerImage;
+            EndScreen.Visibility = Visibility.Visible;
+        }
+        private void TransitionToGameScreen()
+        {
+            EndScreen.Visibility = Visibility.Hidden;
+            TurnPanel.Visibility = Visibility.Visible;
+            GameCanvas.Visibility = Visibility.Visible;
+        }
+        private (Point, Point) FindLinePoints(WinInfo winInfo)
+        {
+            double squareSize = GameGrid.Width / 3;
+            double margin = squareSize / 2;
+
+            if (winInfo.Type == WinType.Row)
+            {
+                double y = winInfo.Number * squareSize + margin;
+                return (new Point(0, y), new Point(GameGrid.Width, y));
+            }
+            if (winInfo.Type == WinType.Column)
+            {
+                double x = winInfo.Number * squareSize + margin;
+                return (new Point(x, 0), new Point(x, GameGrid.Height));
+            }
+            if (winInfo.Type == WinType.MainDiagonal)
+            {
+                return (new Point(0, 0), new Point(GameGrid.Width, GameGrid.Height));
+            }
+            return (new Point(GameGrid.Width, 0), new Point(0, GameGrid.Height));
+        }
+        private void ShowLine(WinInfo winInfo)
+        {
+            (Point start, Point end) = FindLinePoints(winInfo);
+
+            Line.X1 = start.X;
+            Line.Y1 = start.Y;
+
+            Line.X2 = end.X;
+            Line.Y2 = end.Y;
+
+            Line.Visibility = Visibility.Visible;
+        }
         private void OnMoveMade(int r, int c)
         {
             Player player = gameState.GameGrid[r, c];
             imageControls[r, c].Source = imageSources[player];
             PlayerImage.Source = imageSources[gameState.CurrentPlayer];
         }
-        private void OnGameEnded(GameResult gameResult)
+        private async void OnGameEnded(GameResult gameResult)
         {
-
+            await Task.Delay(1000);
+            if (gameResult.Winner == Player.None)
+            {
+                TransitionToEndScreen("Ничья", null);
+            }
+            else
+            {
+                ShowLine(gameResult.WinInfo);
+                TransitionToEndScreen("Победа: ", imageSources[gameResult.Winner]);
+            }
         }
         private void OnGameRestarted()
         {
+            for (int r = 0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    imageControls[r, c].Source = null;
+                }
+            }
 
+            PlayerImage.Source = imageSources[gameState.CurrentPlayer];
+            TransitionToGameScreen();
         }
         private void GameGrid_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -70,9 +135,9 @@ namespace TicTacToe
             gameState.MakeMove(row, col);
         }
 
-        private void Button_Click(object sender, MouseButtonEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            gameState.Reset();
         }
     }
 }
